11bf454e5220e336841bec22aabfdbc3e96397ab
dkang3 <dkang3@ncsu.edu>
2018-06-06 22:14:36 -0500

    KMP

--- KMP.py
+++ KMP.py
@@ -23,13 +23,13 @@ def buildPrefixArray(arr, pattern):
 
     while i < n:
         if pattern[i] == pattern[j]:
+            arr[i] = j + 1
             i += 1
             j += 1
         else:
             if j > 0:
                 j = arr[j - 1]
             elif j == 0:
-                j = 0
                 arr[i] = 0
                 i += 1
 
